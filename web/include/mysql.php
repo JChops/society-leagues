b65e44773d9bd556b037059ec3dc9af05dcc6e2f
Douglas Chimento <douglas.chimento@yodle.com>
2014-12-12 16:47:20 -0500

    Reverting to old do_login and old db password to test docker container

--- web/include/mysql.php
+++ web/include/mysql.php
@@ -1,6 +1,6 @@
 <?php
 if (getenv('DB_HOST')) {
-   $conn = mysql_connect(getenv('DB_HOST'), 'league');
+   $conn = mysql_connect(getenv('DB_HOST'), 'daleegdbuser','pr3dat0r');
    mysql_select_db('league', $conn);
 } else {
    $conn = mysql_connect('localhost', 'league');
